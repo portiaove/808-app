799cc76172e8e0d8dfec910cc6c1c881dcf8bce5
Juan Portillo <juanportillog@gmail.com>
2019-07-05 19:09:43 +0200

    Steps refactored

--- src/components/808/808.js
+++ src/components/808/808.js
@@ -4,13 +4,15 @@ import Step from './Step'
 
 class MachineDrum extends React.Component {
   state = {
+    drums: {
+      kick: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      snare: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      clHat: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      opHat: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      hiTom: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      loTom: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+    },
     bpm: Number,
-    kick: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    snare: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    clHat: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    opHat: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    hiTom: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    loTom: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
     activeDrum: 'kick'
   }
 
@@ -23,6 +25,8 @@ class MachineDrum extends React.Component {
 
   handleStep = (e) => {
     const { activeDrum } = this.state
+    const index = e.target
+    console.log(index)
     console.log(activeDrum)
     this.setState({
       
@@ -31,6 +35,14 @@ class MachineDrum extends React.Component {
 
   render() {
     const {activeDrum} = this.state
+    const {kick} = this.state.drums
+
+    const Steps = kick.map((el, index) => {
+
+      return < Step onClick={this.handleStep} key={index} />
+
+    })
+
     return (
       <div>
         <h1>This Is The Machine Drum</h1>
@@ -43,23 +55,9 @@ class MachineDrum extends React.Component {
           < Drum activeDrum={activeDrum} onClick={this.handleActiveDrum} title="Hi Tom" name="hiTom"/>
         </div>
         <div className="container pt-5 d-flex justify-content-around ">
-          < Step onClick={this.handleStep} i={0} />
-          < Step onClick={this.handleStep} i={1} />
-          < Step onClick={this.handleStep} i={2} />
-          < Step onClick={this.handleStep} i={3} />
-          < Step onClick={this.handleStep} i={4} />
-          < Step onClick={this.handleStep} i={5} />
-          < Step onClick={this.handleStep} i={6} />
-          < Step onClick={this.handleStep} i={7} />
-          < Step onClick={this.handleStep} i={8} />
-          < Step onClick={this.handleStep} i={9} />
-          < Step onClick={this.handleStep} i={10} />
-          < Step onClick={this.handleStep} i={11} />
-          < Step onClick={this.handleStep} i={12} />
-          < Step onClick={this.handleStep} i={13} />
-          < Step onClick={this.handleStep} i={14} />
-          < Step onClick={this.handleStep} i={15} />
-          < Step onClick={this.handleStep} i={16} />
+
+          {Steps}
+
         </div>
       </div>
     )
--- src/components/808/Step.js
+++ src/components/808/Step.js
@@ -3,9 +3,9 @@ import React from 'react'
 class Step extends React.Component {
   
   render() {
-    const {onClick} = this.props
+    const { onClick, key } = this.props
     return(
-      <button onClick={onClick} style={{background: 'yellow', height: '40px', width: '20px'}}>
+      <button onClick={onClick} index={key} style={{background: 'yellow', height: '40px', width: '20px'}}>
         <div style={{background: 'black', height: '9px', width: '9px'}}></div>
 
       </button>

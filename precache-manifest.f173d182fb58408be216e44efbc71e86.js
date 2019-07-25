d8609be744398dba044de0f02232745220acab2e
Juan Portillo <juanportillog@gmail.com>
2019-07-25 11:17:44 +0200

    Updates

---
+++ precache-manifest.f173d182fb58408be216e44efbc71e86.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbbd0fe460437caa289e6c7443668858",
+    "url": "/808-app/index.html"
+  },
+  {
+    "revision": "d90856ce6a9333d67484",
+    "url": "/808-app/static/css/main.efc168f6.chunk.css"
+  },
+  {
+    "revision": "d76027fe0380b9fc9a74",
+    "url": "/808-app/static/js/2.7940bcf5.chunk.js"
+  },
+  {
+    "revision": "d90856ce6a9333d67484",
+    "url": "/808-app/static/js/main.e30bbb2a.chunk.js"
+  },
+  {
+    "revision": "236c08f6bd6a2c2d5119",
+    "url": "/808-app/static/js/runtime~main.60e65678.js"
+  },
+  {
+    "revision": "f7c5fb74fc9bb6275aeb68b17f32463d",
+    "url": "/808-app/static/media/808Icon.f7c5fb74.svg"
+  },
+  {
+    "revision": "0a393246b0f649cc867a701af22b7970",
+    "url": "/808-app/static/media/808Icon2.0a393246.svg"
+  },
+  {
+    "revision": "ca5ca4d1582fc1992937dd8bf109f6a2",
+    "url": "/808-app/static/media/808IconSelected.ca5ca4d1.svg"
+  },
+  {
+    "revision": "f64bca41576b2fe2f8d743a74b0c25ac",
+    "url": "/808-app/static/media/FloppyDisk.f64bca41.svg"
+  },
+  {
+    "revision": "dc99794d7be9dc88285cca51f564cc7f",
+    "url": "/808-app/static/media/FloppyDisk2.dc99794d.svg"
+  },
+  {
+    "revision": "91e83f6235eeb11e2b57d1b041e40901",
+    "url": "/808-app/static/media/FloppyDisk3.91e83f62.svg"
+  },
+  {
+    "revision": "48e3208464e5cc9c0865b0bf712f6269",
+    "url": "/808-app/static/media/GoBack.48e32084.svg"
+  },
+  {
+    "revision": "2c22fca2e6627676b0cd60ec1be3beb2",
+    "url": "/808-app/static/media/Inconsolata-Regular.2c22fca2.ttf"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/808-app/static/media/Montserrat-Regular.a8a11736.ttf"
+  },
+  {
+    "revision": "db3fd90207311aab037130900631ea14",
+    "url": "/808-app/static/media/Oswald-Regular.db3fd902.ttf"
+  },
+  {
+    "revision": "a3649815e40709ca063e69bcf09d5851",
+    "url": "/808-app/static/media/RobotoMono-Regular.a3649815.ttf"
+  },
+  {
+    "revision": "2a3602a78dc56f9b886c65387659b0e0",
+    "url": "/808-app/static/media/holding.2a3602a7.svg"
+  },
+  {
+    "revision": "d088d8371905ecf97cf908c1ae1b79dd",
+    "url": "/808-app/static/media/holdingSelected.d088d837.svg"
+  },
+  {
+    "revision": "ac2850e2483bf1048f91f255db772a85",
+    "url": "/808-app/static/media/pause.ac2850e2.svg"
+  },
+  {
+    "revision": "c14e5e8df2a1643d8513febb21935a76",
+    "url": "/808-app/static/media/play.c14e5e8d.svg"
+  },
+  {
+    "revision": "3b3b14a6551431ea164c4394b8611240",
+    "url": "/808-app/static/media/profile.3b3b14a6.svg"
+  },
+  {
+    "revision": "0204441e2ca225d19f7d97aa8944cdd7",
+    "url": "/808-app/static/media/profileSelected.0204441e.svg"
+  }
+]);

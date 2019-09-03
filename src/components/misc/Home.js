e002ff4cf6a87e8e5e245f1874270e27f50cd2b5
Juan Portillo <juanportillog@gmail.com>
2019-09-03 18:34:19 +0200

    production for heroku

--- src/components/misc/Home.js
+++ src/components/misc/Home.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import { withAuthContext } from '../../contexts/AuthStore'
+import aobeatsLogo from '../../images/aobeats.svg'
+// import './Home.css'
 
-const Home = (props) => {
+class Home extends React.Component {
 
-  if (props.isAuthenticated()) {
+  state = {
+    img: '',
+    login: 'none'
+  }
+
+
+  handleLogin = () => {
+    this.setState({img: 'none', login: ''})
+  }
+
+  render() {
+
+  if (this.props.isAuthenticated()) {
     return < Redirect to='/home' />
   }
+  
 
   return(
     <div className='Box-Wrapper'>
-    <div className='Home'>
+      <img src={aobeatsLogo} alt='aobeats baby' onClick={this.handleLogin} style={{display: this.state.img}}/>
+    <div className='Home' style={{display: this.state.login}}>
       <Link className='Link' to='/login'><button className='btn-Link'>Log in</button></Link>
       <Link className='Link' to='/register'><button className='btn-Link'>Register</button></Link>
     </div>
     </div>
   )
+  }
 }
 
 export default withAuthContext(Home)
